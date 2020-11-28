90400ddc561cd6bc6115217d49efb14450685182
Ricardo Zermeño Chquito <Ricardo.RZC98@gmail.com>
2020-11-28 15:32:35 -0600

    inicioProyecto2

---
+++ app/Producto.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Producto extends Model
+{
+    use SoftDeletes;
+    protected $table = "Producto";
+    protected $filleable = ['id', 'Nombre', 'Descripcion', 'NumeroProductos', 'Precio']
+}
